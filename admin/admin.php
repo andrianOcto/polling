81e1f78c5009c5a11afb2803cb8712a22b0b3208
andrianOcto <andrian.octo@gmail.com>
2014-02-24 21:05:17 +0700

    Benerin andrian ampas

--- admin/admin.php
+++ admin/admin.php
@@ -2,6 +2,7 @@
 mysql_connect('localhost', 'root', '') or die(mysql_error());
 mysql_select_db('polling') or die(mysql_error());
 
+
 session_start();
 //If your session isn't valid, it returns you to the login screen for protection
 if(empty($_SESSION['admin_id'])){
@@ -9,6 +10,7 @@ if(empty($_SESSION['admin_id'])){
 }
 ?>
 <html><head>
+        <script src="js/admin.js"></script>
 <link href="css/admin_styles.css" rel="stylesheet" type="text/css" />
 </head><body bgcolor="tan">
 <center><a href ="https://sourceforge.net/projects/pollingsystem/"><img src = "images/logo" alt="site logo"></a></center><br>     
@@ -21,7 +23,22 @@ if(empty($_SESSION['admin_id'])){
 <p align="center">&nbsp;</p>
 <div id="container">
 
-<p>Click a link above to perform an administrative operation.</p>
+<p>Pick a poll to be modified</p>
+<SELECT NAME="poll" id="poll">
+    <?php
+        $pollings=mysql_query("SELECT * FROM tbpolling")
+        or die("There are no records to display ... \n" . mysql_error());
+        while ($row=mysql_fetch_array($pollings)){
+            echo "<OPTION VALUE=$row[poll_name]>$row[poll_name]";
+        }
+    ?>
+</SELECT>
+
+<button type="button" onclick="createCookie()" >
+    Choose Poll
+</button>
+
+
 
 
 </div>
